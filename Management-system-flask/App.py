a88767c0ec149c9df0c38ed43752e0d85c0f3a00
masingita makhubele <masingita.ebenezer@gmail.com>
2023-11-08 15:24:48 +0200

    adding and viewing employees

--- Management-system-flask/App.py
+++ Management-system-flask/App.py
@@ -1,119 +1,67 @@
-from flask import Flask,url_for,request,redirect
+from flask import Flask,url_for,request
 from flask.templating import render_template
 import os
 from os import system
 import psycopg2
-
-
-# conn = psycopg2.connect(host='localhost',
-#                             database='employee',
-#                             user='postgres',
-#                             port=5430,
-#                             password='')
-# cursor = conn.cursor()
-# cursor.execute("select * from users " )
-
-# books = cursor.fetchall()
-# print(books)
-
+from flask import redirect
+from flask import Flask
+ 
+# Flask constructor takes the name of 
+# current module (__name__) as argument.
 app = Flask(__name__)
-def create_app():
- return app
+ 
+def db_conn():
+     conn = psycopg2.connect(host='localhost',
+                            database='employee',
+                            user='academy_inter_mac24',
+                            port=5430,
+                            password='')
+     return conn
 
-
-def get_db_connection():
-    conn = psycopg2.connect(host='localhost',
-                            database='flask_db',
-                            user=os.environ['DB_USERNAME'],
-                            password=os.environ['DB_PASSWORD'])
-    return conn
-
-
-
+# The route() function of the Flask class is a decorator, 
+# which tells the application which URL should call 
+# the associated function.
 @app.route('/')
+# ‘/’ URL is bound with hello_world() function.
 def index():
-    return render_template('home.html')
+        conn=db_conn()
+        # declaring a cursor
+        cur=conn.cursor()
+        cur.execute("SELECT * from Employee")
+        data=cur.fetchall()
 
-# ...
-
-@app.route('/create/', methods=('GET', 'POST'))
-def addEmployee():
-    if request.method == 'POST':
-        Id = request.form['Id']
-        full_name = request.form['full_name']
-        Email = request.form['email']
-        phone_no= request.form['phone_no']
-        Address = request.form['address']
-        Position= request.form['position']
-        Salary= request.form['salary']
-
-        conn = get_db_connection()
-        cur = conn.cursor()
-        cur.execute('INSERT INTO Employees (Id,full_name,Email,phone_no,Address,Position,Salary)'
-                    'VALUES (%s, %s, %s, %s, %s, %s, %s)',
-                    (Id,full_name,Email,phone_no,Address,Position,Salary))
-        conn.commit()
         cur.close()
         conn.close()
-        return redirect(url_for('index'))
+        return render_template('dashboard.html',data=data)
 
-    return render_template('Add_employee.html')
- 
-@app.route('/form')
-def form():
-    return render_template('form.html')
+@app.route('/dd')
+def hello():
+    print("hello")
+    return render_template('index.html')
+
+@app.route('/create',methods=['POST'])
+def create():
+    conn=db_conn()
+    cur=conn.cursor()
+       
+    if request.method == 'POST':
+       
+        conn=db_conn()
+        cur=conn.cursor()
+        id = request.form.get('id')
+        full_name = request.form.get('inputfullname')
+        email = request.form.get('inputEmail4')
+        phone_no= request.form.get('inputphonenumber')
+        position= request.form.get('inputposition')
+        salary= request.form.get('inputsalary')
+        address = request.form.get('inputAddress')
+
+        cur.execute('''INSERT INTO employee(id,full_name,email,phone_no,position,salary,address) VALUES (%s,%s,%s,%s,%s,%s,%s)''',(id,full_name,email,phone_no,position,salary,address))
+        conn.commit()
+        return render_template('dashboard.html')
 
 
-
-
-
-# @app.route('/')
-# def index():
-#     conn = get_db_connection()
-#     cur = conn.cursor()
-#     cur.execute('SELECT * FROM books;')
-#     books = cur.fetchall()
-#     cur.close()
-#     conn.close()
-#     return render_template('index.html')
-
-# from flask import Flask
-
-# app = Flask(__name__)
-
-# # @app.route("/")
-# # An object of Flask class is our WSGI application.
-# from flask import Flask
- 
-# # Flask constructor takes the name of 
-# # current module (__name__) as argument.
-# app = Flask(__name__)
- 
-# # The route() function of the Flask class is a decorator, 
-# # which tells the application which URL should call 
-# # the associated function.
-# @app.route('/')
-# # ‘/’ URL is bound with hello_world() function.
-# def hello_world():
-#     # if request.method == 'POST':
-#     #     name = request.form['name']
-#     #     age = request.form['age']
-#         conn = psycopg2.connect(host='localhost',
-#                             database='employee',
-#                             user=os.environ['postgres'],
-#                             port=5430,
-#                             password=os.environ[''])
-#         cursor = conn.cursor()
-#         cursor.execute("select * from users where name=%s", )
-#         books = cursor.fetchall()
-        
-#         conn.commit()
-#         cursor.close()
-#         return render_template('index.html', books="books")
 
 if __name__ == '__main__':
-   app.run(debug=True)
-#     # run() method of Flask class runs the application 
-#     # on the local development server.
-#     app.run()
-# # main driver function
+ app.run(debug=True, port=3507)
+
